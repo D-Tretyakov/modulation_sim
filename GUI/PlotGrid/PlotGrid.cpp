734ee6b558be08f59416159178e2bbec321f0e8e
D-Tretyakov <tretyakov.daniil.education@gmail.com>
2022-11-07 01:19:11 +0300

    now drawing grid and ticks on axes

--- GUI/PlotGrid/PlotGrid.cpp
+++ GUI/PlotGrid/PlotGrid.cpp
@@ -1,9 +1,20 @@
 #include "PlotGrid.hpp"
+#include <iostream>
 
 PlotGridPanel::PlotGridPanel(wxPanel* parent)
     : wxPanel(parent)
 {
 
+}
+
+void PlotGridPanel::SetGridUnitSize(int new_size)
+{
+    m_GridUnitSize = new_size;
+}
+
+int PlotGridPanel::GetGridUnitSize()
+{
+    return m_GridUnitSize;
 }
 
 void PlotGridPanel::OnPaintEvent(wxPaintEvent& event)
@@ -20,18 +31,51 @@ void PlotGridPanel::OnSize(wxSizeEvent& event)
 
 void PlotGridPanel::DrawGrid(wxDC& dc)
 {
-    wxSize grid_size = dc.GetSize();
-    int x_center = grid_size.GetWidth() / 2;
-    int y_center = grid_size.GetHeight() / 2;
+    const wxSize grid_size = dc.GetSize();
+    const int grid_width = grid_size.GetWidth();
+    const int grid_height = grid_size.GetHeight();
 
     const int h_offset = 10; // px
     const int v_offset = 10; // px
+    const int tick_size = 6; // px - length of tick  
 
-    dc.SetPen(wxPen(*wxLIGHT_GREY, 2));
+    dc.SetPen(wxPen(m_AxesColor, 2));
     // draw Y axis
-    dc.DrawLine(h_offset, v_offset, h_offset, grid_size.GetHeight() - 10);
+    dc.DrawLine(h_offset, v_offset, h_offset, grid_height - v_offset);
     // draw X axis
-    dc.DrawLine(h_offset, y_center, grid_size.GetWidth() - 10, y_center);
+    dc.DrawLine(h_offset, grid_height / 2, grid_width - h_offset, grid_height / 2);
+
+    // draw grid and ticks on axes
+    // draw ticks from y = 0 to bottom
+    for (int i = grid_height / 2 + m_GridUnitSize; i < (grid_height - v_offset); i += m_GridUnitSize)
+    {
+        // draw tick
+        dc.SetPen(wxPen(m_TicksColor, 1));
+        dc.DrawLine(h_offset - tick_size / 2, i, h_offset + tick_size / 2, i);
+        // draw grid line
+        dc.SetPen(wxPen(m_GridColor, 1));
+        dc.DrawLine(h_offset, i, grid_width - h_offset, i);
+    }
+    // draw ticks from y = 0 to top
+    for (int i = grid_height / 2 - m_GridUnitSize; i > v_offset; i -= m_GridUnitSize)
+    {
+        // draw tick
+        dc.SetPen(wxPen(m_TicksColor, 1));
+        dc.DrawLine(h_offset - tick_size / 2, i, h_offset + tick_size / 2, i);
+        // draw grid line
+        dc.SetPen(wxPen(m_GridColor, 1));
+        dc.DrawLine(h_offset, i, grid_width - h_offset, i);
+    }
+    // draw ticks for X axis
+    for (int i = h_offset + m_GridUnitSize; i < (grid_width - h_offset); i += m_GridUnitSize)
+    {
+        // draw tick
+        dc.SetPen(wxPen(m_TicksColor, 1));
+        dc.DrawLine(i, grid_height / 2 - tick_size / 2, i, grid_height / 2 + tick_size / 2);
+        // draw grid line
+        dc.SetPen(wxPen(m_GridColor, 1));
+        dc.DrawLine(i, v_offset, i, grid_height - v_offset);
+    }
 }
 
 BEGIN_EVENT_TABLE(PlotGridPanel, wxPanel)
--- GUI/PlotGrid/PlotGrid.hpp
+++ GUI/PlotGrid/PlotGrid.hpp
@@ -8,8 +8,17 @@ public:
     void OnPaintEvent(wxPaintEvent& event);
     void OnSize(wxSizeEvent& event);
 
+    void SetGridUnitSize(int new_size);
+    int GetGridUnitSize();
+
     DECLARE_EVENT_TABLE()
 
 private:
     void DrawGrid(wxDC& dc);
+
+    wxColor m_AxesColor = wxColor(0, 0, 0);
+    wxColor m_TicksColor = wxColor(0, 0, 0);
+    wxColor m_GridColor = wxColor(125, 125, 125);
+
+    int m_GridUnitSize = 20; // in pixels
 };
